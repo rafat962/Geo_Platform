f2b91a2f5b061fd76c8defd10ca734b4ee930a6d
HebaKhayyal <Hkhayyal@jeddah.gov.sa>
2025-05-20 12:08:33 +0300

    TRANSLATION

--- src/environment/devolpmentApi.js
+++ src/environment/devolpmentApi.js
@@ -3,6 +3,8 @@ const client_secret = "667dd1feca3c421da2ce612655c229bc";
 const BASE_URL =
     "https://services9.arcgis.com/ONBxkqBd9gmR0qpr/arcgis/rest/services/";
 
-const backendUrl = "http://127.0.0.1:8000/api/v1/";
-const backendImagesUrl = "http://127.0.0.1:8000/";
+const backendUrl = "http://102.46.144.34:80/api/v1/";
+const backendImagesUrl = "http://102.46.144.34:80/";
+// const backendUrl = "http://127.0.0.1:8000/api/v1/";
+// const backendImagesUrl = "http://127.0.0.1:8000/";
 export { client_id, client_secret, BASE_URL, backendUrl, backendImagesUrl };
