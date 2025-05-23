4e9f1c3105ab8b54af2ee3e46cb3de2ed733e4b5
rafat962 <rafatkamel96@gmail.com>
2025-05-23 18:24:42 +0300

    BEFOR-SALAH-START

--- src/components/LandUses/LandUses.jsx
+++ src/components/LandUses/LandUses.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { useMap } from "../../shared/hooks/useMap";
-
+import styles from "./LandUses.module.css";
 const LandUses = () => {
     const { viewRef } = useMap();
     return (
         <div className="w-full h-full ">
-            s<div ref={viewRef} className="w-full h-full"></div>
+            <div
+                ref={viewRef}
+                className={`w-full h-full ${styles.bgred}`}
+            ></div>
         </div>
     );
 };
