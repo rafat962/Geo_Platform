de97493e198d459b0c407069e54496a4b604b177
HebaKhayyal <Hkhayyal@jeddah.gov.sa>
2025-05-23 17:49:02 +0300

    FISRT

--- src/components/LandUses/LandUses.jsx
+++ src/components/LandUses/LandUses.jsx
@@ -5,6 +5,7 @@ const LandUses = () => {
     const { viewRef } = useMap();
     return (
         <div className="w-full h-full ">
+            asd
             <div ref={viewRef} className="w-full h-full"></div>
         </div>
     );
